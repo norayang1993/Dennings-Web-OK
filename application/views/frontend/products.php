f8e2dc961cac24d0c739beb6c9dbcc4a8fc8746d
fdjkgh580_notbook <fdjkgh580@yahoo.com.tw>
2016-12-20 17:59:22 +0800

    建構

---
+++ application/views/frontend/products.php
@@ -0,0 +1,55 @@
+<div class="container">
+    
+    <div class="banner">
+        Banner
+    </div>
+    
+    <div class="product">
+    
+        <div class="protab_site">
+            <div class="item">
+                Pocket Clause
+            </div>
+            <div class="item">
+                1Flashlight
+            </div>
+        </div>
+
+        <div class="content">
+
+            <div class="proinfo">
+                <header>
+                    <div class="icon">icon</div>
+                    <h1>Pocket Clause</h1>
+                    <div class="linksite">
+                        <a href="">Apple Store</a>
+                        <a href="">Google Play</a>
+                        <a href="">Website</a>
+                    </div>
+                </header>
+                <div class="content">
+                    <p>Dennings Limited, its affiliates, subsidiaries and parent company (collectively, “we”, “us”, “our” or “Dennings”) provides services to you. Your use of any of Dennings services, websites, applications, software or contents (collectively, the “Dennings Service”) provided on, from or through any of our application or websites and/or their forwarding websites (“Website”) is subject to these terms of service (the “Terms of Service”) in effect at the time of your use. Dennings reserves the right to update and change these Terms of Service from time to time without notice or acceptance by you, so please check this page frequently for updates and changes.</p>
+                    <p>Your use of the Dennings Service is also subject to the Dennings Privacy Policy in effect at the time of your use, which is incorporated herein. You can review the most current version of the Dennings Privacy Policy at this website.</p>
+                </div>
+            </div>
+
+            <div class="proinfo">
+                <header>
+                    <div class="icon">icon</div>
+                    <h1>1Flashlight (Android) / Bible Verses Flashlight (iOS)</h1>
+                    <div class="linksite">
+                        <a href="">Apple Store</a>
+                        <a href="">Google Play</a>
+                    </div>
+                </header>
+                <div class="content">
+                    <p>Dennings Limited, its affiliates, subsidiaries and parent company (collectively, “we”, “us”, “our” or “Dennings”) provides services to you. Your use of any of Dennings services, websites, applications, software or contents (collectively, the “Dennings Service”) provided on, from or through any of our application or websites and/or their forwarding websites (“Website”) is subject to these terms of service (the “Terms of Service”) in effect at the time of your use. Dennings reserves the right to update and change these Terms of Service from time to time without notice or acceptance by you, so please check this page frequently for updates and changes.</p>
+                    <p>Your use of the Dennings Service is also subject to the Dennings Privacy Policy in effect at the time of your use, which is incorporated herein. You can review the most current version of the Dennings Privacy Policy at this website.</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    
+
+</div>
