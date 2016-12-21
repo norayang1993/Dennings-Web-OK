a97011e902cb4b5dcc8b5fa945036c5880b87597
fdjkgh580_notbook <fdjkgh580@yahoo.com.tw>
2016-12-21 15:40:59 +0800

    partners 手機版

---
+++ application/views/frontend/partners.php
@@ -0,0 +1,65 @@
+<div class="container">
+
+    <div class="plist">
+
+        <div class="company">
+            <div class="logosite">
+                <div class="logo original" style="background-image: url('images/partners/logo-origin.png')"></div>
+            </div>
+
+            <div class="desc">
+                <h1 class="title">
+                    <a class="text" href="">
+                        Origin Business (Hong Kong and Kaohsiung)
+                        <i class="point"></i>
+                    </a>
+                </h1>
+                <div class="content">
+                    <p>Origin Business is led by Mr. Johnson Lam. From company incorporation, board meeting, to protect your intellectual properties in trademark, design and patent, Origin Business support our clients’ growth with its elite group of professionals, and utilize the expertises across the group of companies. Johnson is your business partner.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="company">
+            <div class="logosite">
+                <div class="logo hoceans" style="background-image: url('images/partners/logo-hoceans.png')"></div>
+            </div>
+
+            <div class="desc">
+                <h1 class="title">
+                    <a class="text" href="">
+                        Hoceans (Hong Kong)
+                        <i class="point"></i>
+                    </a>
+                </h1>
+                <div class="content">
+                    <p>Our dedicated Legal and IT recruitment team is under ‘HOCEANS’ led by Regina Kwok (“Gina”) and Ricky Lau. Gina is a lawyer. Ricky is an IT and App expert. The team’s legal recruit has a strong focus in HK and PRC. IT recruits go wherever our clients go and both supported by the Specialist China recruitment team Topview.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="company">
+            <div class="logosite">
+                <div class="logo topview" style="background-image: url('images/partners/logo-topview.png')"></div>
+            </div>
+
+            <div class="desc">
+                <h1 class="title">
+                    <a class="text" href="">
+                        Topview (Beijing)
+                        <i class="point"></i>
+                    </a>
+                </h1>
+                <div class="content">
+                    <p>The Specialist China recruitment brand in China is ‘TOPVIEW’, led by Ms. McQueen Qin, a renowned and experienced recruiter who never gives up on the toughest case, and is bold enough to advise clients on the good or bad of candidates. The team is growing, and currently into 8 members, with office in Da Xing district.</p>
+                </div>
+            </div>
+        </div>
+        
+
+
+
+    </div>
+</div>
