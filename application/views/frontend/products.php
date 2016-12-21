ab9a38f9f4ebd21083230a3aaa07841d03e68616
fdjkgh580_notbook <fdjkgh580@yahoo.com.tw>
2016-12-21 14:24:41 +0800

    產品完成

--- application/views/frontend/products.php
+++ application/views/frontend/products.php
@@ -1,39 +1,67 @@
 <div class="container">
     
     <div class="banner">
-        Banner
     </div>
     
     <div class="product">
-    
+
         <div class="protab_site">
-            <div class="item">
+            <button class="item">
                 Pocket Clause
-            </div>
-            <div class="item">
+            </button>
+            <button class="item">
                 1Flashlight
-            </div>
+            </button>
         </div>
 
-        <div class="content">
+        <div class="pdolist">
 
-            <div class="proinfo">
-                <header>
-                    <div class="icon">icon</div>
-                    <h1>Pocket Clause</h1>
-                    <div class="linksite">
-                        <a href="">Apple Store</a>
-                        <a href="">Google Play</a>
-                        <a href="">Website</a>
+            <article class="proinfo">
+                <header class="pheader">
+                    <i class="icon" style="background-image: url('images/products/PocketClause.png')"></i>
+                    <div class="tinfo">
+                        <div class="outwrap herver_center outer">
+
+                            <div class="inwrap herver_center inner">
+                                <h1 class="ptit ">Pocket Clause</h1>
+                                <div class="linksite">
+                                    <a class="link" href="">Apple Store</a>
+                                    <a class="link" href="">Google Play</a>
+                                    <a class="link" href="">Website</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </header>
-                <div class="content">
+                <div class="ctt">
                     <p>Dennings Limited, its affiliates, subsidiaries and parent company (collectively, “we”, “us”, “our” or “Dennings”) provides services to you. Your use of any of Dennings services, websites, applications, software or contents (collectively, the “Dennings Service”) provided on, from or through any of our application or websites and/or their forwarding websites (“Website”) is subject to these terms of service (the “Terms of Service”) in effect at the time of your use. Dennings reserves the right to update and change these Terms of Service from time to time without notice or acceptance by you, so please check this page frequently for updates and changes.</p>
                     <p>Your use of the Dennings Service is also subject to the Dennings Privacy Policy in effect at the time of your use, which is incorporated herein. You can review the most current version of the Dennings Privacy Policy at this website.</p>
                 </div>
-            </div>
+            </article>
 
-            <div class="proinfo">
+            <article class="proinfo">
+                <header class="pheader">
+                    <i class="icon" style="background-image: url('images/products/1Flashlight.png')"></i>
+                    <div class="tinfo">
+                        <div class="outwrap herver_center outer">
+
+                            <div class="inwrap herver_center inner">
+                                <h1 class="ptit ">1Flashlight (Android) / Bible Verses Flashlight (iOS)</h1>
+                                <div class="linksite">
+                                    <a class="link" href="">Apple Store</a>
+                                    <a class="link" href="">Google Play</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <div class="ctt">
+                    <p>Dennings Limited, its affiliates, subsidiaries and parent company (collectively, “we”, “us”, “our” or “Dennings”) provides services to you. Your use of any of Dennings services, websites, applications, software or contents (collectively, the “Dennings Service”) provided on, from or through any of our application or websites and/or their forwarding websites (“Website”) is subject to these terms of service (the “Terms of Service”) in effect at the time of your use. Dennings reserves the right to update and change these Terms of Service from time to time without notice or acceptance by you, so please check this page frequently for updates and changes.</p>
+                    <p>Your use of the Dennings Service is also subject to the Dennings Privacy Policy in effect at the time of your use, which is incorporated herein. You can review the most current version of the Dennings Privacy Policy at this website.</p>
+                </div>
+            </article>
+
+            <!-- <div class="proinfo">
                 <header>
                     <div class="icon">icon</div>
                     <h1>1Flashlight (Android) / Bible Verses Flashlight (iOS)</h1>
@@ -43,10 +71,8 @@
                     </div>
                 </header>
                 <div class="content">
-                    <p>Dennings Limited, its affiliates, subsidiaries and parent company (collectively, “we”, “us”, “our” or “Dennings”) provides services to you. Your use of any of Dennings services, websites, applications, software or contents (collectively, the “Dennings Service”) provided on, from or through any of our application or websites and/or their forwarding websites (“Website”) is subject to these terms of service (the “Terms of Service”) in effect at the time of your use. Dennings reserves the right to update and change these Terms of Service from time to time without notice or acceptance by you, so please check this page frequently for updates and changes.</p>
-                    <p>Your use of the Dennings Service is also subject to the Dennings Privacy Policy in effect at the time of your use, which is incorporated herein. You can review the most current version of the Dennings Privacy Policy at this website.</p>
                 </div>
-            </div>
+            </div> -->
 
         </div>
     </div>
